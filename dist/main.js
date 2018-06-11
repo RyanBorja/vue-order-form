9dac906ac37625cdcc9d6fb84b40c2fe14e20ac0
Janine Hempy <jhempy@gmail.com>
2018-06-11 15:21:25 -0400

    Adds styling and click behavior

--- dist/main.js
+++ dist/main.js
@@ -1,5 +1,46 @@
 var demo = new Vue({
 
-	el: '#main'
+	el: '#main',
+
+	data: {
+
+		services: [
+			{
+				name: "Web Development",
+				price: 300,
+				active: true
+			},
+			{
+				name: "Design",
+				price: 400,
+				active: false
+			},
+			{
+				name: "Integration",
+				price: 250,
+				active: true
+			},
+			{
+				name: "Training",
+				price: 220,
+				active: false
+			}
+		]
+
+	},
+
+	methods: {
+
+		total: function() {
+
+			return 0;
+
+		},
+
+		toggleActive: function(s) {
+			s.active = !s.active;
+		}
+
+	}
 
 });
